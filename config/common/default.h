d647e4742144647e5a4457030fdd727de0f67e98
hunterliao29 <hunterliao29@gmail.com>
2024-09-20 01:03:18 -0500

    init

---
+++ config/common/default.h
@@ -0,0 +1,30 @@
+#if !defined (MAP_LAYER_BASE)
+  #define MAP_LAYER_BASE MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_EXTRA)
+  #define MAP_LAYER_EXTRA MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_TAP)
+  #define MAP_LAYER_TAP MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_BUTTON)
+  #define MAP_LAYER_BUTTON MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_NAV)
+  #define MAP_LAYER_NAV MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_MOUSE)
+  #define MAP_LAYER_MOUSE MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_MEDIA)
+  #define MAP_LAYER_MEDIA MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_NUM)
+  #define MAP_LAYER_NUM MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_SYM)
+  #define MAP_LAYER_SYM MAP_LAYER
+#endif
+#if !defined (MAP_LAYER_FUN)
+  #define MAP_LAYER_FUN MAP_LAYER
+#endif
